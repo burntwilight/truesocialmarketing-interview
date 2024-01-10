4151a914ae0adbda2788d149982497d48d294652
Dakota Bradford <dakotagreybradford@gmail.com>
2024-01-10 13:06:37 -0700

    Refactored existing form.

--- src/sections/contactPage/ContactForm.tsx
+++ src/sections/contactPage/ContactForm.tsx
@@ -1,147 +1,106 @@
-import React, { useState } from "react";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import axios from "axios";
-import { useForm } from "react-hook-form";
-import { API_URL } from "@/constants";
+import React, { useState } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { API_URL } from '@/constants';
 
 type FormData = {
-  name: string;
-  email: string;
-  subject: string;
-  message: string;
-  phoneNumber: string;
+    name: string;
+    email: string;
 };
 
 const schema = yup.object().shape({
-  name: yup.string().required("Name is required"),
-  email: yup.string().email("Invalid email").required("Email is required"),
-  phoneNumber: yup
-    .string()
-    .required("Phone number is required")
-    .test(
-      "phone-number-length",
-      "Phone number should be 10 digits",
-      (value) => {
-        console.log(value);
-        return value?.replace(/\D/g, "").length === 10;
-      }
-    ),
-  subject: yup.string().required("Subject is required"),
-  message: yup.string().required("Message is required"),
+    name: yup.string().required('Name is required'),
+    email: yup.string().email('Invalid email').required('Email is required'),
 });
 
 const ContactInner = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [showSuccess, SetShowSuccess] = useState(false);
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-    watch,
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
+    const [isLoading, setIsLoading] = useState(false);
+    const [showSuccess, SetShowSuccess] = useState(false);
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+        watch,
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
 
-  const onSubmit = async (data: FormData) => {
-    setIsLoading(true);
+    const onSubmit = async (data: FormData) => {
+        setIsLoading(true);
 
-    try {
-      const { name, email, subject, message, phoneNumber } = data;
+        try {
+            const { name, email } = data;
 
-      const formData = new FormData();
-      formData.set("your-subject", subject);
-      formData.set("your-name", name);
-      formData.set("your-email", email);
-      formData.set("phoneNumber", phoneNumber);
-      formData.append("your-message", message);
-      const response = await axios.post(API_URL.contactForm, formData);
+            const formData = new FormData();
 
-      if (response.status === 200) {
-        showMessage();
-        reset();
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+            formData.set('your-name', name);
+            formData.set('your-email', email);
 
-  const showMessage = () => {
-    SetShowSuccess(true);
+            const response = await axios.post(API_URL.contactForm, formData);
 
-    setTimeout(() => {
-      SetShowSuccess(false);
-    }, 5000);
-  };
+            if (response.status === 200) {
+                showMessage();
+                reset();
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
-  return (
-    <section id="contact-form" className="container w-full max-w-xs mx-auto p-6 bg-white rounded shadow-md">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label className="block mb-2 text-lg font-semibold text-gray-800">
-          Name
-        </label>
-        <input
-          type="text"
-          {...register("name")}
-          className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:border-blue-500"
-        />
-        <p className="text-red-600">{errors.name?.message}</p>
+    const showMessage = () => {
+        SetShowSuccess(true);
 
-        <label className="block mb-2 text-lg font-semibold text-gray-800">
-          Email
-        </label>
-        <input
-          type="text"
-          {...register("email")}
-          className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:border-blue-500"
-        />
-        <p className="text-red-600">{errors.email?.message}</p>
+        setTimeout(() => {
+            SetShowSuccess(false);
+        }, 5000);
+    };
 
-        <label className="block mb-2 text-lg font-semibold text-gray-800">
-          Phone Number
-        </label>
-        <input
-          type="text"
-          {...register("phoneNumber")}
-          className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:border-blue-500"
-        />
-        <p className="text-red-600">{errors.phoneNumber?.message}</p>
-
-        <label className="block mb-2 text-lg font-semibold text-gray-800">
-          Subject
-        </label>
-        <input
-          type="text"
-          {...register("subject")}
-          className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:border-blue-500"
-        />
-        <p className="text-red-600">{errors.subject?.message}</p>
-
-        <label className="block mb-2 text-lg font-semibold text-gray-800">
-          Message
-        </label>
-        <textarea
-          {...register("message")}
-          className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:border-blue-500"
-        ></textarea>
-        <p className="text-red-600">{errors.message?.message}</p>
-
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 focus:outline-none"
+    return (
+        <section
+            id='contact-form'
+            className='container w-full max-w-xs mx-auto p-6'
         >
-          {isLoading ? "Sending..." : "Submit"}
-        </button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label className='block mb-2 text-lg font-semibold text-gray-800'>
+                    Name
+                </label>
+                <input
+                    type='text'
+                    {...register('name')}
+                    className='w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:border-blue-500'
+                />
+                <p className='text-red-600'>{errors.name?.message}</p>
 
-        {showSuccess && (
-          <p className="mt-4 text-green-600">Message sent successfully!</p>
-        )}
-      </form>
-    </section>
-  );
-}
+                <label className='block mb-2 text-lg font-semibold text-gray-800'>
+                    Email
+                </label>
+                <input
+                    type='text'
+                    {...register('email')}
+                    className='w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:border-blue-500'
+                />
+                <p className='text-red-600'>{errors.email?.message}</p>
+
+                <button
+                    type='submit'
+                    className='w-full text-black hover:text-gray-600 px-4 py-2 rounded-md focus:outline-none mt-4'
+                >
+                    {isLoading ? 'Sending...' : 'Submit'}
+                </button>
+
+                {showSuccess && (
+                    <p className='mt-4 text-green-600'>
+                        Message sent successfully!
+                    </p>
+                )}
+            </form>
+        </section>
+    );
+};
 
 export default ContactInner;
